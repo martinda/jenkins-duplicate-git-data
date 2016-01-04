fb1e6a83d83a906766e780b134028552b08c1e2c
Martin d'Anjou <martin.danjou14@gmail.com>
2016-01-04 12:14:27 -0500

    Use the general checkout step

--- flow.groovy
+++ flow.groovy
@@ -1,4 +1,4 @@
 node('remote') {
-  git url: 'https://github.com/allegro/axion-release-plugin.git' 
-  git url: 'https://github.com/martinda/gradle-simple-plugin.git' 
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'ScmName', name: 'axion-release-plugin']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/allegro/axion-release-plugin.git']]])
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'ScmName', name: 'gradle-simple-plugin']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/martinda/gradle-simple-plugin.git']]])
 }
